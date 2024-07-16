9861a34ebbe82bdb4b2ea04015265c3c9c8e0faf
Roman <romankormylouk@gmail.com>
2024-07-16 13:44:24 +0100

    Add header to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:3000"
+      origins "http://localhost:3000", "http://127.0.0.1:3000"
   
       resource "*",
         headers: :any,
