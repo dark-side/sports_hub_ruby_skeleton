e774f120220723a8a19abdbd002acc9a0ddd96af
Roman Triska <triskaroman@gmail.com>
2024-08-02 14:14:27 +0300

    Update naming for DB's and configs after repo rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "xhub_backend_production"
+  # config.active_job.queue_name_prefix = "ruby_be_genai_plgrnd_production"
 
   config.action_mailer.perform_caching = false
 
