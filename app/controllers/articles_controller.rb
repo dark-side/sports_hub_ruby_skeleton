7d59daf1f4c9abc4d4e2e32a554b26eb789baa55
Roman Kormylo <ro.kormylo@gmail.com>
2025-03-09 20:08:01 +0000

    Add JSON response to articles/{id}

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,29 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
 
-        render json: @articles.as_json(methods: [:image_url, :article_likes, :article_dislikes, :comments_content, :comments_count])
+    render json: article_json(@articles)
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+
+    render json: article_json(@article)
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: article_json(@article), status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
     end
-    
-    def show
-        @article = Article.find(params[:id])
+  end
 
-        render json: url_for(@article.image)
-    end
+  private
 
-    def create
-        @article = Article.new(article_params)
-
-        if @article.save
-            render json: @article, status: :created
-        else
-            render json: @article.errors, status: :unprocessable_entity
-        end
-    end
+  def article_json(article)
+    article.as_json(methods: [:image_url, :article_likes, :article_dislikes, :comments_content, :comments_count])
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001"
+      origins "http://localhost:3000", "http://127.0.0.1:3000",
+              "http://localhost:3001", "http://localhost:3002",
+              "http://0.0.0.0:3001", "http://0.0.0.0:3002", 
+              "http://0.0.0.0:4200", "http://localhost:4200",
+              "http://127.0.0.1:4200"
+
   
       resource "*",
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true,
-        max_age: 86400
+        credentials: true
     end
   end
